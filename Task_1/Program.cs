758f312a4d5617359e7a402586b12be71afffb03
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-20 10:10:14 +0300

    Полиндром

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+if (numA % 100 == ((numA /1000)+9))
+{
+   Console.WriteLine("{0} Палиндром" , numA);
+}
+else 
+{
+    Console.WriteLine("{0} Не палиндром", numA);
+}
+
