39a275392165e3bfc899613c25f3e63f6f38e7f2
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-20 10:56:57 +0300

    Таблица кубов

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()?? " ");
+int count = 1;
+int res = 0;
+while (count <= num)
+{
+    res = count * count * count;
+    Console.WriteLine($"{count}^3 = {res}");
+    count++; 
+}
+
+
+
